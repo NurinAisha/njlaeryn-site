f6ca4c44a6b67670b7d26948bde0bf1f0818b1d9
NurinAisha <nurin2588@gmail.com>
2025-02-10 20:13:17 +0800

    Add website.js

---
+++ website.js
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nurin's Portfolio</title>
+  <link rel="stylesheet" href="website.css">
+</head>
+<body>
+  <div class="main-container">
+    <nav class="navbar">
+  <ul>
+    <li>&nbsp;</li>
+    <li><a href="#hero">Home</a></li>
+    <li><a href="#about">About Me</a></li>
+    <li><a href="#artwork">Portfolio</a></li>
+    <li><a href="#contact">Contact Me</a></li>
+  </ul>
+</nav>
+
+<!-- Hero Section -->
+<section id="hero" class="section hero-content">
+  <div class="hero-text">
+	  
+	  <h1 class="nurin-text">Nurin's</h1><br>
+    <h1 class="portfolio-text">portfolio</h1><br><br><br>
+    <p><span class="sentence-text"></span></p>
+    <p class="sentence-textt"></p><br><br>
+  </div>
+    <div class="image-popper">
+	 <div id="deco1">
+        <img src="TOP LEFT WEB.png" alt="Image 1" class="pop-image">
+	 </div>
+		 
+	 <div id="deco3">
+        <img src="BOTTOM RIGHT WEB.png" alt="Image 3" class="pop-image">
+	 </div> 
+    </div>
+</section>
+
+<!-- About Section -->
+<section id="about" class="section about-content">
+  <div class="images">
+    <img src="Untitled design.png" alt="Placeholder Image">
+  </div>
+
+  <div class="info-container">
+    <div class="info-column">
+      <h2>Who Am I?</h2>
+      <p>I'm Nurin Aisha Najla, born on 17th March 2005, currently studying in UiTM for Mass Communication and Media Studies under MC112. Live in Malaysia, Kuala Lumpur.</p>
+    </div>
+    <div class="info-column">
+      <h2>Skills</h2>
+      <p>I specialize in photo editing, video editing, website design, and game development, constantly sharpening my skills through Adobe softwares such as Dreamweaver, Illustrator, After effects, etc. and I also enhancing my ability in Unreal Engine 5 for game development. Besides that, I have huge social ability to talk publicly without feeling ashame and full of confidence. Along with insane critical thinking by brainstorming unique and original ideas to gain originality and strong personal brand.</p>
+    </div>
+    <div class="info-column">
+      <h2>Goals</h2>
+      <p>I aim to deliver work that ensures client satisfaction and positions me as a trustworthy freelancer to encourage my client to have me in their work needs in the near future.</p>
+    </div>
+    <div class="info-column">
+      <h2>Experience</h2>
+      <p>Since my youth, I’ve held a passion for photo editing and creative media, driving me to pursue in Media Studies. I also once worked as Customer Service Staffs which improving my social skills, work ethics, critical thinking and many more.</p>
+    </div>
+  </div>
+</section>
+
+<!-- Artwork Section -->
+<section id="artwork" class="section artwork-content">
+  <div class="artwork-grid">
+    <div class="artwork-item">
+      <img src="MY ROOM.png" alt="Artwork 1">
+    </div>
+    <div class="artwork-item">
+      <img src="njlaeryn bubble font.png" alt="Artwork 2">
+    </div>
+    <div class="artwork-item">
+      <img src="DSC_0952.JPG" alt="Artwork 3">
+    </div>
+    <div class="artwork-item">
+      <img src="DSC_0971.JPG" alt="Artwork 4">
+    </div>
+    <div class="artwork-item">
+      <img src="NURIN AISHA NAJLA_2024527719_MC1121A.png" alt="Artwork 5">
+    </div>
+    <div class="artwork-item">
+      <img src="DSC_0896.JPG" alt="Artwork 6">
+    </div>
+    <div class="artwork-item">00
+      <img src="lol.jpg" alt="Artwork 7">
+    </div>
+    <div class="artwork-item">
+      <img src="DSC_0953.JPG" alt="Artwork 8">
+    </div>
+  </div>
+</section>
+
+<!-- Contact Section -->
+<section id="contact" class="section contact-content">
+  <div>
+    <div class="lets">
+      <h2>INTERESTED? CONTACT ME NOW!</h2>
+    </div>
+    <ul class="button-list">
+      <li><a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCHrjnFMBssSdSrQnCtCBDVLxqBxtKbjjfmgsdQTRRLNSghnrscxfHnKwQclzVTNSsgdGMZL" target="_blank"><button class="contact-button">SEND ME AN EMAIL</button></a></li>
+      <li><a href="https://www.instagram.com/njlaeryn/?hl=en" target="_blank"><button class="contact-button">VISIT MY INSTA</button></a></li>
+      <li><a href="https://www.facebook.com/nurinaisha.najla" target="_blank"><button class="contact-button">VISIT MY FACEBOOK</button></a></li>
+    </ul>
+  </div>
+	
+		  <!-- Image containers -->
+  <div class="image-container">
+   <img src="NURINS2.jpg" alt="Left Image" class="fade-image left-img">
+  </div>
+	
+</section>
+	</div>
+	
+  <script src="website.js"></script>
+</body>
+</html>
